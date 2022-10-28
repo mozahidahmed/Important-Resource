008c0560669ef5dc83b4f74b30fc576086b9ef89
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-10-28 19:09:39 +0600

    git clone remove git repo

---
+++ GitClone.js
@@ -0,0 +1,9 @@
+//  1.git clone 
+//  2.git status 
+//  3.rmdir .git 
+//  4.press Y 
+//  5.git init 
+// add . commit 
+// git remote add origin https://github.com/mozahidahmed/Important-document.git 
+// git push origin master
+
